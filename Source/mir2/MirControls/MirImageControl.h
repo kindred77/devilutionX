6ba7a8e9516f19c1f8aadebbf62833c85d53a65b
kindred77 <109327586@qq.com>
2024-04-28 20:29:52 +0800

    modify

--- Source/mir2/MirControls/MirImageControl.h
+++ Source/mir2/MirControls/MirImageControl.h
@@ -1,14 +1,31 @@
 #pragma once
 
 #include "mir2/MirControls/MirControl.h"
+#include "mir2/Common/Point.h"
 
 namespace devilution {
 
 class MirImageControl
 using MirImageControlPtr = std::shared_ptr<MirImageControl>;
 
-class MirImageControl : public MirControl, public std::enable_shared_from_this<MirImageControl> {
+class MirImageControl : public MirControl, public std::enable_shared_from_this<MirImageControl>
+{
+private:
+	bool isUseOffSet_;
 
+protected:
+	bool GetIsUseOffset()
+	{
+		return isUseOffSet_;
+	}
+
+public:
+	override PointPtr GetDisplayLocation()
+	{
+		return GetIsUseOffset() ? base.DisplayLocation.Add(Library.GetOffSet(Index)) : base.DisplayLocation; 
+	}
+
+	
 };
 
 }
