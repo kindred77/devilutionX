c817b2cd4998609bd3eedeb2534708432522e645
tangye <109327586@qq.com>
2024-05-09 18:26:49 +0800

    modify

--- Source/mir2/MirControls/MirImageControl.cpp
+++ Source/mir2/MirControls/MirImageControl.cpp
@@ -22,6 +22,20 @@ PointPtr MirImageControl::GetDisplayLocation()
     }
 }
 
+void MirImageControl::SetIsUseOffset(bool isUseOffSet)
+{
+    if (isUseOffSet_ == isUseOffSet) return;
+    isUseOffSet_ = isUseOffSet;
+    OnUseOffSetChanged();
+}
+
+void MirImageControl::OnUseOffSetChanged()
+{
+    MirControl::OnLocationChanged();
+    //if (UseOffSetChanged != null)
+    //    UseOffSetChanged.Invoke(this, EventArgs.Empty);
+}
+
 int MirImageControl::GetIndex()
 {
     return index_;
@@ -55,9 +69,54 @@ void OnLibraryChanged()
     //    LibraryChanged.Invoke(this, EventArgs.Empty);
 }
 
+bool MirImageControl::GetIsDrawImage()
+{
+    return isDrawImage_;
+}
+
+void MirImageControl::SetIsDrawImage(bool isDrawImage)
+{
+    if (isDrawImage_ == isDrawImage) return;
+    isDrawImage_ = isDrawImage;
+    OnDrawImageChanged();
+}
+
+void MirImageControl::OnDrawImageChanged()
+{
+    MirControl::Redraw();
+    //if (DrawImageChanged != null)
+    //    DrawImageChanged.Invoke(this, EventArgs.Empty);
+}
+
+bool MirImageControl::GetIsPixelDetect()
+{
+    return isPixelDetect_;
+}
+
+void MirImageControl::SetIsPixelDetect(bool isPixelDetect)
+{
+    if (isPixelDetect_ == isPixelDetect) return;
+    isPixelDetect_ = isPixelDetect;
+    OnPixelDetectChanged();
+}
+
+void MirImageControl::OnPixelDetectChanged()
+{
+    MirControl::Redraw();
+    //if (PixelDetectChanged != null)
+    //    PixelDetectChanged.Invoke(this, EventArgs.Empty);
+}
+
+
 MirLibPtr MirImageControl::GetMirLib()
 {
     return mirLib_;
+}
+
+SizePtr MirImageControl::GetSize() const
+{
+    if (Library != null && Index >= 0) return Library.GetTrueSize(Index);
+    return base.Size;
 }
 
 }
--- Source/mir2/MirControls/MirImageControl.h
+++ Source/mir2/MirControls/MirImageControl.h
@@ -13,22 +13,37 @@ class MirImageControl : public MirControl, public std::enable_shared_from_this<M
 {
 private:
 	bool isUseOffSet_;
+	bool isDrawImage_;
+	bool isPixelDetect_;
 	int index_;
 	MirLibPtr mirLib_;
 
 protected:
 	bool GetIsUseOffset();
+	void OnUseOffSetChanged();
 	void OnIndexChanged();
 	void OnLibraryChanged();
+	void OnDrawImageChanged();
+	void OnPixelDetectChanged();
 
 public:
 	PointPtr GetDisplayLocation() override;
 
+	void SetIsUseOffset(bool isUseOffSet);
+
 	virtual int GetIndex();
 	virtual void SetIndex(int index);
 
+	bool GetIsDrawImage();
+	void SetIsDrawImage(bool isDrawImage);
+
+	bool GetIsPixelDetect();
+	void SetIsPixelDetect(bool isPixelDetect);
+
 	void SetMirLib(MirLibPtr mirLib);
 	MirLibPtr GetMirLib();
+
+	SizePtr GetSize() const override;
 
 };
 
